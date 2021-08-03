ea93282064a445b8c440ee4fff064d8f0022b863
Miguel-DeGracia21 <87886781+Miguel-DeGracia21@users.noreply.github.com>
2021-08-03 18:43:20 -0500

    Add files via upload

---
+++ Programa 6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <locale.h>
+
+int main()
+{
+    char seguir;
+    int i, numero;
+
+    do
+    {
+        printf( "\n   Introduzca un n%cmero entero: ", 163 );
+        scanf( "%d", &numero );
+
+        printf( "\n   La tabla de multiplicar del %d es:\n", numero );
+
+        /* Inicio del anidamiento */
+
+        for ( i = 1 ; i <= 12 ; i++ )
+            printf( "\n   %d * %d = %d", i, numero, i * numero );
+
+        /* Fin del anidamiento */
+
+        printf( "\n\n   %cDesea ver otra tabla (s/n)?: ", 168 );
+        fflush( stdin );
+        scanf( "%c", &seguir );
+
+    } while ( seguir != 'n' );
+
+    return 0;
+}
